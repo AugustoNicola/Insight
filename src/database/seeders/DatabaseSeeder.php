20a696dc9eb7914cce3863d17ee5ecb14f5817c3
lambda <augusp64@gmail.com>
2021-12-16 01:58:10 -0300

    :electric_plug: Modelos: Factories y Seeders

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,16 +3,40 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
+use App\Models\Usuarie;
+use App\Models\Categoria;
+use App\Models\Publicacion;
+use App\Models\Comentario;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+    
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Usuarie::factory()
+            ->count(10)
+            ->create();
+        
+        Categoria::factory()
+            ->count(4)
+            ->create();
+        
+        Publicacion::factory()
+            ->count(20)
+            ->usuarieExistente()
+            ->create()
+            ->each(function ($publicacion) {
+                $publicacion->categorias()->attach(Categoria::all()->random(rand(1, 4))->pluck('id')->toArray());
+                
+                $publicacion->reacciones()->attach(Usuarie::all()->random(rand(1, 7))->pluck('id')->toArray(), ["relacion" => Arr::random(["me_gusta", "guardar"])]);
+            });
+        
+            Comentario::factory()
+                ->count(20)
+                ->publicacionExistente()
+                ->usuarieExistente()
+                ->create();
     }
 }
