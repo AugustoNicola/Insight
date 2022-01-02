f42dd5e21a195214990366b57fdd3cbecac1b159
lambda <augusp64@gmail.com>
2022-01-02 00:49:40 -0300

    :art: Estructura Blade: App + errores

--- src/resources/views/entrar.blade.php
+++ src/resources/views/entrar.blade.php
@@ -1,27 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Entrar Test</title>
-</head>
-<body>
-	@if ($errors->any())
-		@foreach ($errors->all() as $error)
-			<p>{{ $error }}</p>
-		@endforeach
-	@endif
+@extends("app")
+
+@section("titulo", "Iniciar Sesión")
+
+@section('contenido')
+<form method="POST">
+	<h1 class="text-3xl font-bold underline">
+		Hello world!
+	</h1>
 	
-	<form method="POST">
-		@csrf
-		<label for="nombre">nombre</label>
-		<input type="text" name="nombre" id="nombre" value={{old("nombre")}}>
-		
-		<label for="contrasena">Contraseña</label>
-		<input type="text" name="contrasena" id="contrasena" value={{old("contrasena")}}>
-		
-		<button type="submit">Enviar!</button>
-	</form>
-</body>
-</html>
+	@csrf
+	<label for="nombre">nombre</label>
+	<input type="text" name="nombre" id="nombre" value={{old("nombre")}}>
+	
+	<label for="contrasena">Contraseña</label>
+	<input type="text" name="contrasena" id="contrasena" value={{old("contrasena")}}>
+	
+	<button type="submit">Enviar!</button>
+</form>
+@endsection
