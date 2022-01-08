935899581b7e3ed5c12d067558f43fa900fa248c
lambda <augusp64@gmail.com>
2022-01-08 17:00:14 -0300

    :mag: Ruta Publicación: Tests

---
+++ src/tests/Feature/Publicaciones/RutaPublicacionTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature\Usuaries;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Illuminate\Support\Arr;
+
+use App\Models;
+
+class RutaPublicacionTest extends TestCase
+{
+    use RefreshDatabase; // uso RefreshDatabase para que el default sea sin publicaciones
+
+    public function test_DeberiaMostrarPaginaNoEncontrado_CuandoPublicacionNoExiste()
+    {
+        // no fue creada ninguna publicacion
+
+        $response = $this->get("/publicacion/1234");
+        $response->assertStatus(404); // 404: Not found
+    }
+
+    public function test_DeberiaMostrarInfo_CuandoExistePublicacion()
+    {
+        $publicacion = Models\Publicacion::factory()->create();
+        $categorias = Models\Categoria::factory()->count(3)->create();
+        Models\Usuarie::factory()->count(2)->create();
+
+        $publicacion->categorias()->attach(Models\Categoria::all()->pluck('id')->toArray());
+        $publicacion->reacciones()->attach(Models\Usuarie::all()->random(2)->pluck('id')->toArray(), ["relacion" => "me_gusta"]);
+
+        $response = $this->get("/publicaciones/" . $publicacion->id);
+
+        $response->assertStatus(200); // 200: Ok
+        $response->assertSessionHasNoErrors();
+
+        $response->assertSeeText($publicacion->titulo);
+        foreach ($categorias as $categoria) {
+            $response->assertSeeText($categoria->nombre);
+        }
+        $response->assertSeeText("Por " . $publicacion->autore->nombre);
+        $response->assertSeeText($publicacion->fecha_creacion->format("d/m/y"));
+        $response->assertSeeText("3 me gusta");
+        $response->assertSeeText("0 comentarios");
+        $response->assertSeeText($publicacion->cuerpo);
+    }
+
+    public function test_DeberiaMostrarMensajeComentariosVacio_CuandoNoHayComentariosRelacionados()
+    {
+        $publicacion = Models\Publicacion::factory()->create();
+        Models\Categoria::factory()->count(3)->create();
+        $publicacion->categorias()->attach(Models\Categoria::all()->pluck('id')->toArray());
+
+        // no hay comentarios para la publicacion
+
+        $response = $this->get("/publicaciones/" . $publicacion->id);
+
+        $response->assertStatus(200); // 200: Ok
+        $response->assertSessionHasNoErrors();
+
+        $response->assertSeeText("No se han encontrado comentarios de esta publicación.");
+    }
+
+    public function test_DeberiaMostrarComentarios_CuandoHayComentariosRelacionados()
+    {
+        $publicacion = Models\Publicacion::factory()->create();
+        Models\Categoria::factory()->count(3)->create();
+        $publicacion->categorias()->attach(Models\Categoria::all()->pluck('id')->toArray());
+
+        $comentarios = Models\Comentario::factory()->usuarieExistente()->count(3)->create();
+
+        $response = $this->get("/publicaciones/" . $publicacion->id);
+
+        $response->assertStatus(200); // 200: Ok
+        $response->assertSessionHasNoErrors();
+
+        $response->assertDontSeeText("No se han encontrado comentarios de esta publicación.");
+
+        foreach ($comentarios as $comentario) {
+            $response->assertSeeText($comentario->usuarie->nombre);
+            $response->assertSeeText($comentario->fecha_creacion->format("d/m/y"));
+            $response->assertSeeText($comentario->cuerpo);
+        }
+    }
+}
