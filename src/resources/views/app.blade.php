adfc1abcaf9e8d935f299523df057d1ef4a92548
lambda <augusp64@gmail.com>
2022-01-19 02:19:14 -0300

    :notebook: :art: Footer: Creación y Estilos Mobile

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -13,16 +13,23 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    {{--<link rel="dns-prefetch" href="//fonts.gstatic.com">--}}
     {{--<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">--}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600;700&family=Libre+Franklin:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Noticia+Text:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <!-- Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 </head>
-<body>
-	<main>
-		@include("utilidades.errores")
-		@yield("contenido")
-	</main>
+    <body class="bg-fondo">
+    @include("utilidades.errores")
+    
+    @yield("contenido")
+    
+    @include("utilidades.footer")
 </body>
 </html>
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,11 +1,25 @@
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        colors: {
+            primario: "#075E52",
+            beige: "#647C76",
+            azul: "#30557D",
+            celeste: "#6587B2",
+            fondo: "#C9C9C9",
+            blanco: "#FFFFFF",
+            blancohover: "#B0B0B0",
+        },
+        fontFamily: {
+            titulo: ["Domine", "sans-serif"],
+            texto: ["Noticia Text", "serif"],
+            ui: ["Libre Franklin", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
