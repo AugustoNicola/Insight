86eccbc72dc02a1a0229f1bb8e30c629433f7ecb
lambda <augusp64@gmail.com>
2022-01-24 02:13:30 -0300

    :art: RutaRegistrarse: Estilos on primer preview imagen

--- src/resources/views/paginas/registrarse.blade.php
+++ src/resources/views/paginas/registrarse.blade.php
@@ -1,22 +1,27 @@
 @extends("app")
 
-@section("titulo", "Iniciar Sesión")
+@section("titulo", "Crear Cuenta")
 
 @section('contenido')
-<form method="POST" enctype="multipart/form-data">
-	<h1>Crear Cuenta</h1>
+<h1 class="my-6 font-titulo font-bold text-center text-negro text-3xl">Crear Cuenta</h1>
+<form method="POST" enctype="multipart/form-data" class="lg:max-w-screen-sm lg:mx-auto px-4 pb-6 flex flex-col flex-nowrap gap-10">
+	<input type="text" name="nombre" id="nombre" placeholder="Nombre de Cuenta" class="bg-fondo font-ui text-2xl border-b-[3px] border-gris">
+	<input type="password" name="contrasena" id="contrasena" placeholder="Contraseña" class="bg-fondo font-ui text-2xl border-b-[3px] border-gris">
+	<input type="password" name="contrasena_confirmation" id="contrasena_confirmation" placeholder="Repetir Contraseña" class="bg-fondo font-ui text-2xl border-b-[3px] border-gris">
 	
-	<input type="text" name="nombre" id="nombre" placeholder="Nombre de Cuenta">
-	<input type="password" name="contrasena" id="contrasena" placeholder="Contraseña">
-	<input type="password" name="contrasena_confirmation" id="contrasena_confirmation" placeholder="Repetir Contraseña">
-	
-	<div class="contenedor-imagen">
-		<label for="imagen">Imagen de cuenta</label>
-		<input type="file" name="imagen" id="imagen" />
-		<img id="preview-imagen" />
+	<div class="flex flex-col lg:flex-row flex-wrap lg:flex-nowrap justify-center items-center gap-4 lg:gap-10">
+		<p class="font-ui font-semibold text-center text-xl text-negro">Imagen de cuenta</p>
+		<label for="inputImagen">
+			<img id="previewImagen" class="w-24 h-24 rounded-full ring-offset-2 ring-4 ring-primario cursor-pointer bg-grisoscuro"/>
+		</label>
+		<input type="file" name="imagen" id="inputImagen" class="hidden"/>
 	</div>
 	
 	@csrf
-	<button type="submit">Registrate</button>
+	<div class="flex flex-row flex-nowrap justify-end items-center">
+		<button type="submit" class="w-fit mt-3 px-6 py-2 font-ui font-medium text-2xl bg-primario hover:bg-primariohover rounded-md">Registrate</button>
+	</div>
 </form>
-	@endsection
+
+<script src="{{ asset('js/registrarse.js') }}"></script>
+@endsection
