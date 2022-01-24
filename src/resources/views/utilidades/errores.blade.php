60fa762b029d639ddac9bc79f329235b6e3d1a6a
lambda <augusp64@gmail.com>
2022-01-24 01:25:59 -0300

    :art: Utilidad Errores estilizada

--- src/resources/views/utilidades/errores.blade.php
+++ src/resources/views/utilidades/errores.blade.php
@@ -1,5 +1,7 @@
 @if(count($errors) > 0)
+<div class="mt-4 flex flex-col flex-nowrap gap-3">
 	@foreach($errors->all() as $error)
-		<p>{{$error}}</p>
+	<p class="mx-3 px-3 py-2 block bg-errorfondo text-errortexto text-ui text-center text-xl rounded-xl">{{$error}}</p>
 	@endforeach
+</div>
 @endif
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
             primariopastel: "#D2E8E8",
             primariomedio: "#8CBBBB",
             primariofondo: "#E0EEEB",
+            errortexto: "#D32F2F",
+            errorfondo: "#FFCDD2",
             beige: "#647C76",
             azul: "#30557D",
             celeste: "#6587B2",
